0e6df0ee232d5036a973d09554336b60a84abf8b
3D-Shareware <kevingbeyer@gmail.com>
2026-02-04 21:43:49 -0600

    Finalized.

--- evan-and-kevin-only-1/main.gd
+++ evan-and-kevin-only-1/main.gd
@@ -1,1 +1,29 @@
 extends Node
+
+enum scenes {title, game, dex}
+
+var scene_collection = [preload("res://levels/title.tscn"), preload("res://levels/game_runner.tscn"), preload("res://levels/dex.tscn")]
+
+var current_scene: Node
+
+var fullscreen = false
+
+var beat_game = false
+
+func _ready() -> void:
+	change_scene(scenes["title"])
+
+func change_scene(scene_id: int):
+	if current_scene != null:
+		current_scene.queue_free()
+	var new_scene = scene_collection[scene_id].instantiate()
+	add_child(new_scene)
+	current_scene = new_scene
+
+func toggle_fullscreen():
+	if fullscreen:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		fullscreen = false
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		fullscreen = true
